1352c75bfb018326f651ff535b6f85d5fb16a7cb
Boris Shaposhnikov <bshaposhnikov01@gmail.com>
2021-01-28 11:16:27 +0300

    - target dir

--- msqueue/target/generated-sources/annotations/msqueue/generated/MSQueueBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package msqueue.generated;
-public class MSQueueBenchmark_jmhType extends MSQueueBenchmark_jmhType_B3 {
-}
-
--- stack-elimination/target/generated-sources/annotations/stack/generated/StackBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package stack.generated;
-public class StackBenchmark_jmhType extends StackBenchmark_jmhType_B3 {
-}
-
